cd65d107c23ec53ee9c925f36a62cc8639e16a57
Beth Zabinski <bxz3519@gmail.com>
2013-11-22 00:17:07 -0500

    User edit, update, index, destroy

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131120051518) do
+ActiveRecord::Schema.define(version: 20131122003709) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20131120051518) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
